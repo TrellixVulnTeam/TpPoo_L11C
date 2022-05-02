dac2496c427bf1d8cf4509dbe8e7045fb5ec6818
folongemerson <folongemersonwell@gmail.com>
2022-05-02 23:42:28 +0100

    message of the commit

---
+++ assets/js/classList.js
@@ -0,0 +1,15 @@
+//list of main class are going to be use inside this program
+alert("the classList")
+function testClass(){
+    alert("inclusion is working")
+}
+
+class Aliment{
+
+
+    constructor(){
+
+    }
+}
+
+export default {testClass}
---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$.getScript("classList.js",function(){
+    alert("well import")
+})
+$(document).ready(()=>{
+alert("end of loading")
+
+})
+
+
+
+// $(document).ready(function(){
+//     $("p").click(function(){
+//       $(this).hide();
+//     });
+//   });
