a5f5ea7480e111bff28df9e7f591badc0a80da81
folongemerson <folongemersonwell@gmail.com>
2022-05-14 20:55:24 +0100

    fin de la creation des class aliments

--- assets/js/classList.js
+++ assets/js/classList.js
@@ -15,15 +15,76 @@ function testClass(){
 
 //element chimique
 //[magnésium,phosphore,potassium,fer,sodium,calsium]
+
+/* bon a savoir
+1g de proteine =17kj = 4kcal
+1g lipide = 38kj = 4kcal
+1g glucide = 17kj = 4kcal
+*/
+
 class Aliment{
 
     name = "nom de l'aliment"
-    constructor(name,
-        calory,vitamine
-        ){
+    valeurEnergetique
+    cholesterol
+    sodium
+    fibre
+    lipide
+    sucre
+    amidon
+    proteine
+
+    constructor(name,calory,vitamine)
+    {
         // On trouve des calories dans les aliments et boissons qui contiennent des glucides, des protéines et/ou des lipides.
         this.vitamine = vitamine
         this.calory = calory
         this.name = name
+    }
+    afficheramidon(nom){
+        alert(this.amidon)
+    }
+}
+
+
+class Mineraux{
+    calcium
+    fer
+    phosphore
+    constructor(calcium,fer,phosphore){
+        this.phosphore = phosphore
+        this.fer  = fer
+        this.calcium = calcium
+    }
+
+    valeurEnergetique(){
+        return 17+38+17
+    }
+}
+
+
+class vitamine{
+    B1
+    B2
+    B6
+    B9
+    B12
+    PP
+    C
+    D
+    E
+    A
+    constructor(B1,B2,B6,B9,B12,PP,C,D,E,A)
+    {
+        this.B1=B1
+        this.B2=B2
+        this.B6=B6
+        this.B9=B9
+        this.B12=B12
+        this.PP= PP
+        this.C = C
+        this.D = D
+        this.E = E
+        this.A = A
     }
 }
